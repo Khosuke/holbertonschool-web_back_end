fe2bc7040ce76a4b4bbe26f60a4441fea8e9a164
Khosuke <9814@holbertonstudents.com>
2025-03-27 14:33:44 +0100

    task 1 eslint workaround

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
-export default function taskBlock(trueOrFalse) {
-  if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
-  }
-
+export default function taskBlock() {
   var task = false;
   var task2 = true;
-  
+
   return [task, task2];
 }
