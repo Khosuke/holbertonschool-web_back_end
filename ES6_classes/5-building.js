6faaf8f0b516e0cb8667e9a04d90b2f76757e3a8
Khosuke <9814@holbertonstudents.com>
2025-03-28 21:54:56 +0100

    change typo in the throw error message

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Parent must implement evacuationWarningMessage');
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
