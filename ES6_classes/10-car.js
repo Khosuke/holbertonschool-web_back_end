d09662087c2d9f08e0ca4ad5c0048734e7a6fef3
Khosuke <9814@holbertonstudents.com>
2025-03-28 23:05:21 +0100

    Change attribute cycle to color

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,8 +1,8 @@
 export default class Car {
-  constructor(brand = undefined, motor = undefined, cycle = undefined) {
+  constructor(brand = undefined, motor = undefined, color = undefined) {
     this._brand = brand;
     this._motor = motor;
-    this._cycle = cycle;
+    this._color = color;
   }
 
   cloneCar() {
