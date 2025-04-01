9f04e6c203f8f0e51ff0c71662871ba25a6e8c4c
Khosuke <9814@holbertonstudents.com>
2025-04-01 13:52:48 +0200

    Create a function named groceriesList that returns a map of groceries with the following items (name, quantity)

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const mapList = new Map();
+  mapList.set('Apples', 10);
+  mapList.set('Tomatoes', 10);
+  mapList.set('Pasta', 1);
+  mapList.set('Rice', 1);
+  mapList.set('Banana', 5);
+  return mapList;
+}
