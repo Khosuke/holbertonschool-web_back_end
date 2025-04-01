b5b6d5d9ec5e3a103ad791f4b895e07aee84f3c8
Khosuke <9814@holbertonstudents.com>
2025-04-01 13:57:35 +0200

    Return nothing when startString is empty

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
   let cleaned = '';
+  if (startString === '') {
+    return cleaned
+  }
   for (const element of set) {
     if (element.startsWith(startString)) {
       cleaned += `${element.slice(startString.length)}-`;
