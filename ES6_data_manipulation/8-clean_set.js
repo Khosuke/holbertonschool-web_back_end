d193b97db330586b463bd5ec88d89ec7c943c2d3
Khosuke <9814@holbertonstudents.com>
2025-04-01 15:09:22 +0200

    Change for loop to a return with filter, map and join

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,8 @@
 export default function cleanSet(set, startString) {
-  let cleaned = '';
-  if (startString === '') {
-    return cleaned
+  if (!startString || typeof startString !== 'string') {
+    return '';
   }
-  for (const element of set) {
-    if (element.startsWith(startString)) {
-      cleaned += `${element.slice(startString.length)}-`;
-    }
-  }
-  return cleaned.slice(0, -1);
+  return [...set].filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
 }
