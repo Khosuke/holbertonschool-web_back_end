7e83cf4aa1e3a4e71467c15a0a213b6a5eb78a2a
Khosuke <9814@holbertonstudents.com>
2025-04-02 14:41:31 +0200

    remove console log of then response of promise

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,9 +3,7 @@ import createUser from "./4-user-promise.js";
 // eslint-disable-next-line
 import uploadPhoto from "./5-photo-reject.js";
 
-
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const response = Promise.allSettled([createUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => console.log(values));
+  const response = Promise.allSettled([createUser(firstName, lastName), uploadPhoto(fileName)]);
   return response;
 }
