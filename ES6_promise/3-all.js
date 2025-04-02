71ab7a5e5c890cad1fb4ff238ed303327caa6c62
Khosuke <9814@holbertonstudents.com>
2025-04-02 12:09:29 +0200

    change to remove braces inside .then() function

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,8 +3,6 @@ import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
+    .then(([photo, user]) => console.log(`${photo.body} ${user.firstName} ${user.lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
