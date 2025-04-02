3c36ab8157e8649d7baa54d0c162815aaf39be81
Khosuke <9814@holbertonstudents.com>
2025-04-02 15:39:28 +0200

    Fix typo in verification of the status (fullfield -> fulfilled)

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,9 +5,11 @@ import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const response = Promise.allSettled([createUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
+  .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fullfield' ? result.value : result.reason
-    })))
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }
+  ))
+  );
   return response;
 }
