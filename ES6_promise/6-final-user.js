10e607cf449fad2e3b18974910b1aa6f34e52cbf
Khosuke <9814@holbertonstudents.com>
2025-04-02 14:39:37 +0200

    Return a list of promise with their status and value

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,6 +4,8 @@ import createUser from "./4-user-promise.js";
 import uploadPhoto from "./5-photo-reject.js";
 
 
-export default function handleProfileSignup(firstName, lastName,fileName) {
-  return Promise.all([uploadPhoto(fileName), createUser(firstName, lastName)])
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const response = Promise.allSettled([createUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => console.log(values));
+  return response;
 }
