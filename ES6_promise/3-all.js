80f885a8bbc483acd06a07fd5a623485aa0c002f
Khosuke <9814@holbertonstudents.com>
2025-04-02 11:45:56 +0200

    Handle multiple successful promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+import { uploadPhoto, createUser } from "./utils.js";
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
