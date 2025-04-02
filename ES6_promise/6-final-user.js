4705b8d424ebec594b8c8b58deb57c9305b4f9c4
Khosuke <9814@holbertonstudents.com>
2025-04-02 14:53:12 +0200

    Map the promise result to be an array of status and value

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,6 +4,10 @@ import createUser from "./4-user-promise.js";
 import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const response = Promise.allSettled([createUser(firstName, lastName), uploadPhoto(fileName)]);
+  const response = Promise.allSettled([createUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fullfield' ? result.value : result.reason
+    })))
   return response;
 }
