50230916888b3aaf73b241a9eb9a7b926fdae43d
Khosuke <9814@holbertonstudents.com>
2025-04-02 15:42:25 +0200

    Change value in case of rejected status to have only the reason.message

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,12 +4,10 @@ import createUser from "./4-user-promise.js";
 import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const response = Promise.allSettled([createUser(firstName, lastName), uploadPhoto(fileName)])
-  .then((results) => results.map((result) => ({
+  return Promise.allSettled([createUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.status === 'fulfilled' ? result.value : result.reason.message,
     }
-  ))
-  );
-  return response;
+    )));
 }
