83f701daa016e185f3166e67d44f0f49d20a9880
Khosuke <9814@holbertonstudents.com>
2025-05-22 14:11:14 +0200

    Task 1, program that uses process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+function getUserInput() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+  });
+
+  process.on('SIGINT', () => {
+    process.exit();
+  });
+
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
+}
+
+getUserInput();
