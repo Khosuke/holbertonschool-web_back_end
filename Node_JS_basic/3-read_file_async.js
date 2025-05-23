5e1403746b4fb627e4eff9956761649603b464af
Khosuke <9814@holbertonstudents.com>
2025-05-23 23:19:35 +0200

    Added file for task 3, Reading a file asynchronously with Node JS

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,37 @@
+const fs = require('fs').promises;
+
+async function countStudents(dbPath) {
+  try {
+    const data = await fs.readFile(dbPath, 'utf-8');
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+
+    const headers = lines[0].split(',');
+    const fieldIndex = headers.indexOf('field');
+
+    const students = {};
+
+    for (let i = 1; i < lines.length; i++) {
+      const columns = lines[i].split(',');
+      const field = columns[fieldIndex].trim();
+      const firstname = columns[0].trim();
+
+      if (field && firstname) {
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+      }
+    }
+
+    process.stdout.write(`Number of students: ${lines.length - 1}\n`);
+
+    for (const [field, names] of Object.entries(students)) {
+      process.stdout.write(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`);
+    }
+
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
